5e4f3ac817c9aa7bcbd29256bd813493a1e919f6
lc <347458713@qq.com>
2020-11-18 20:30:17 +0800

    test

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pthoncode.calculator import Calculator
 
 
@@ -17,17 +19,20 @@ class TestCalc:
     def teardown_class(self):
         print("计算结束")
 
-    def test_add(self):
+    @pytest.mark.parametrize('a,b,expect', [
+        [1, 1, 2], [100, 100, 200], [0.1, 0.1, 0.2], [-1, -1, -2], [1, 0, 1]
+    ], ids=['int_case', 'bignum_case', 'float_case', 'minus_case', 'zero_case'])
+    def test_add(self, a, b, expect):
         # calc = Calculator()
-        result = self.calc.add(1, 1)
-        assert result == 2
+        result = self.calc.add(a, b)
+        assert expect == result
 
-    def test_add1(self):
-        # calc = Calculator()
-        result = self.calc.add(100, 100)
-        assert result == 200
-
-    def test_add2(self):
-        # calc = Calculator()
-        result = self.calc.add(0.1, 0.1)
-        assert result == 0.2
+    # def test_add1(self):
+    #     # calc = Calculator()
+    #     result = self.calc.add(100, 100)
+    #     assert result == 200
+    #
+    # def test_add2(self):
+    #     # calc = Calculator()
+    #     result = self.calc.add(0.1, 0.1)
+    #     assert result == 0.2
